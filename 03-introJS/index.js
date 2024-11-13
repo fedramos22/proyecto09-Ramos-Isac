378718f5234bf3dd34d4d332f975023963d0f013
Victoria Isac <viki.isac120599@gmail.com>
2024-11-13 11:56:28 -0300

    Delete 03-introJS directory

--- 03-introJS/index.js
+++
@@ -1,9 +0,0 @@
-let prueba = "hola grupo"
-
-/* operadores matematicos */
-
-let numeroUno = 25
-let numeroDos = 31
-
-let resultado = numeroUno + numeroDos
-console.log ("El resultado es: " + resultado)
